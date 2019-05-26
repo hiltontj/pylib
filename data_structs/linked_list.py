30a39de84118046fdb302dd23a54eb58f5780fe3
hiltontj <trevor.hilton@gmail.com>
2019-05-26 02:06:06 -0400

    add iteration to linked list and test it

--- data_structs/linked_list.py
+++ data_structs/linked_list.py
@@ -5,15 +5,29 @@ class Node:
 
 class LinkedList:
     def __init__(self):
-        self.head = None
+        self._head = None
+    
+    def __iter__(self):
+        self._loc = self._head
+        return self
+
+    def __next__(self):
+        if self._loc is None:
+            raise StopIteration
+        result = self._loc.data
+        self._loc = self._loc.next
+        return result
     
     def push(self, val):
-        temp = self.head
-        self.head = Node(val)
+        temp = self._head
+        self._head = Node(val)
         if temp is not None:
-            self.head.next = temp
+            self._head.next = temp
 
     def pop(self):
-        temp = self.head
-        self.head = self.head.next
+        temp = self._head
+        self._head = self._head.next
         return temp.data
+
+    def head(self):
+        return self._head.data
--- tests/linked_list.py
+++ tests/linked_list.py
@@ -2,7 +2,6 @@
 from .context import data_structs as ds
 import unittest
 
-
 class TestLinkedList(unittest.TestCase):
     def test_strings(self):
         ll = ds.LinkedList()
@@ -12,6 +11,21 @@ class TestLinkedList(unittest.TestCase):
         self.assertEqual(ll.pop(), "three")
         self.assertEqual(ll.pop(), "two")
         self.assertEqual(ll.pop(), "one")
+
+    def test_access(self):
+        ll = ds.LinkedList()
+        ll.push("one")
+        self.assertEqual(ll.head(), "one")
+    
+    def test_iteration(self):
+        expected = ["one", "two"]
+        ll = ds.LinkedList()
+        ll.push(expected[0])
+        ll.push(expected[1])
+        i = len(expected) - 1
+        for n in ll:
+            self.assertEqual(expected[i], n)
+            i = i - 1
 
 if __name__ == "main":
     unittest.main()
